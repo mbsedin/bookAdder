4ecc8b2482c5c008c2b036e91e077d95ac27072a
Massamba-Bienvenu <boukamass@gmail.com>
2022-02-03 06:27:36 +0100

    LOCAL STORAGE DONE

--- appES6.js
+++ appES6.js
@@ -13,11 +13,11 @@ class UI {
     const row = document.createElement("tr");
     // Insert cols
     row.innerHTML = `
-        <td>${book.title}</td>
-        <td>${book.author}</td>
-        <td>${book.isbn}</td>
-        <td><a href="#" class="delete">X<a></td>
-      `;
+          <td>${book.title}</td>
+          <td>${book.author}</td>
+          <td>${book.isbn}</td>
+          <td><a href="#" class="delete">X<a></td>
+        `;
     list.appendChild(row);
   }
   showAlert(message, className) {
@@ -51,6 +51,48 @@ class UI {
   }
 }
 
+// LOCAL STORAGE CLASS
+class Store {
+  //get from localstorage
+  static getBooks() {
+    let books;
+    if (localStorage.getItem("books") === null) {
+      books = [];
+    } else {
+      books = JSON.parse(localStorage.getItem("books"));
+    }
+    return books;
+  }
+
+  static displayBooks() {
+    const books = Store.getBooks();
+    books.forEach((book) => {
+      const ui = new UI();
+      //add book to UI
+      ui.addBookToList(book);
+    });
+  }
+
+  // add to localstorage
+  static addBooks(book) {
+    const books = Store.getBooks();
+    books.push(book);
+    localStorage.setItem("books", JSON.stringify(books));
+  }
+
+  static removeBook(isbn) {
+    const books = Store.getBooks();
+    books.forEach((book, index) => {
+      if (book.isbn === isbn) {
+        books.splice(index, 1);
+      }
+    });
+    localStorage.setItem("books", JSON.stringify(books));
+  }
+}
+// DOM Load event
+document.addEventListener("DOMContentLoaded", Store.displayBooks());
+
 // Event Listener for add book
 document.getElementById("book-form").addEventListener("submit", function (e) {
   // Get form values
@@ -72,6 +114,9 @@ document.getElementById("book-form").addEventListener("submit", function (e) {
     // Add book to list
     ui.addBookToList(book);
 
+    // Add to local storage
+    Store.addBooks(book);
+
     // Show success
     ui.showAlert("Book Added!", "success");
 
@@ -90,6 +135,8 @@ document.getElementById("book-list").addEventListener("click", function (e) {
   // Delete book
   ui.deleteBook(e.target);
 
+  // Remove from local storage
+  Store.removeBook(e.target.parentElement.previousElementSibling.textContent);
   // Show message
   ui.showAlert("Book Removed!", "success");
 
